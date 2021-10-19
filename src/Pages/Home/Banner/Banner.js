155bb13c8a2f3d266dbbc3fddfca3d7b43a2f4ec
Md. Akramul Hoque <ictianlikhon6@gmail.com>
2021-10-19 09:02:51 +0600

    header

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Banner.css'
 const Banner = () => {
     return (
-        <div className='banner d-flex  align-items-center'>
-            <div className="container ">
+        <div className='banner d-flex align-items-center'>
+            <div className="container">
                 <div className="row">
-                    <div className="col-md-10">
-                        <h1 className="banner-title" >Take care of your Health Now</h1>
-                        <h3 className="sub-title">WE WILL HELP YOU TO BECOME HEALTHY</h3>
-                        
+                    <div className="col-md-8">
+                        <h1 className="banner-title" >High Innovative Technlogy & Professional Dentists</h1>
+                        <p className="sub-title">Dr. Joseph Phillips is an active member of the Academy of  General Dentistry and strives to stay up to date with the latest  in techniques and technology in the dental profession.</p>
+
                     </div>
-                    <div className="col-md-2"></div>
+                    <div className="col-md-4"></div>
                 </div>
             </div>
         </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -3,32 +3,36 @@ import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 import { HashLink } from 'react-router-hash-link';
+import logo from '../../../images/logo-medi.jpg';
+import './Header.css'
 
 const Header = () => {
-  const {user,logOut}=useAuth()
-    return (
-        <>
-        <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
-          <Container>
-          <Navbar.Brand href="#home">MediCare</Navbar.Brand>
+  const { user, logOut } = useAuth()
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
           <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-            <Nav.Link as={HashLink} to="/home#services">Services</Nav.Link>
-            <Nav.Link as={HashLink} to="/doctor">Doctor</Nav.Link> 
-           { user?.email ?
-           <Button onClick={logOut} variant="light">Log Out</Button>:
-            <Nav.Link as={Link} to="/login">Login</Nav.Link> }
+          <Navbar.Collapse className="justify-content-end">
+            <Nav.Link className="text-light" as={HashLink} to="/home">Home</Nav.Link>
+            <Nav.Link className="text-light" as={HashLink} to="/services">Services</Nav.Link>
+            <Nav.Link className="text-light" as={HashLink} to="/doctor">Doctor</Nav.Link>
 
-        <Navbar.Text>
-            Signed in as: <a href="#login">{user?.displayName}</a>
-        </Navbar.Text>
-        </Navbar.Collapse>       
-          </Container>
-        </Navbar>
-       
-      </>
-    );
+            <Navbar.Text>
+              <a className="text-decoration-none text-muted me-2" href="#login">{user?.displayName}</a>
+            </Navbar.Text>
+            {user?.email ?
+              <Button onClick={logOut} variant="light">Log Out</Button> :
+              <Nav.Link className="text-light" as={Link} to="/login">Login</Nav.Link>}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+    </>
+  );
 };
 
 export default Header;
